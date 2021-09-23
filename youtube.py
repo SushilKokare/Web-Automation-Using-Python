c282f7111a34da7452135ef69637c6db5637ec26
SushilKokare <sushil4403kokare@gmail.com>
2021-09-23 13:46:56 +0530

    first commit

---
+++ youtube.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Problem occured : after click() function google chrome opens and automatically closes
+# for this problem follow link to get solution https://www.reddit.com/r/selenium/comments/g3nhz6/browser_closes_right_after_finishing_test/
+
+class Music():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        chrome_options = Options() 
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    
+    def play(self):
+        #name=input("Enter a youtube channel name : ")
+        self.driver.get('https://www.youtube.com/c/MumbikerNikhil/videos')
+
+       # new=self.driver.find_element_by_xpath('//*[@id="thumbnail"]')
+        new=self.driver.find_element_by_xpath('//*[@id="items"]/ytd-grid-video-renderer[1]')
+        new.click()
+       
+    
+bot = Music()
+bot.play()
